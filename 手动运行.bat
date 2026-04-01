9b0ea1fd4adaccfb719dd34f93f3d01d9c585df0
Qingsong Liu <112749498+Liu-Qing-song@users.noreply.github.com>
2026-04-01 13:44:13 +0800

    Update 手动运行.bat

--- 手动运行.bat
+++ 手动运行.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul
 echo ========================================
 echo GitHub Trending 邮件推送 - 手动运行
 echo ========================================
-cd /d "C:\Users\Z00575KC\Desktop\LQS\Others\Github daily trending"
+cd /d "%~dp0"
 python main.py --once
 echo.
 echo 按任意键退出...
